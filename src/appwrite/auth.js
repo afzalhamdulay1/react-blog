0031fca44deedc380a6004de0e87191f6dd9727a
afzal hamdulay <afzalhamdulay1@gmail.com>
2024-11-11 20:35:15 +0400

    removed old commented code from appwrite auth

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,67 +1,3 @@
-// import conf from '../conf/conf.js';
-// import { Client, Account, ID } from "appwrite";
-
-
-// export class AuthService {
-//     client = new Client();
-//     account;
-
-//     constructor() {
-//         this.client
-//             .setEndpoint(conf.appwriteUrl)
-//             .setProject(conf.appwriteProjectId);
-//         this.account = new Account(this.client);            
-//     }
-
-//     async createAccount({email, password, name}) {
-//         try {
-//             const userAccount = await this.account.create(ID.unique(), email, password, name);
-//             if (userAccount) {
-//                 // call another method
-//                 return this.login({email, password});
-//             } else {
-//                return  userAccount;
-//             }
-//         } catch (error) {
-//             throw error;
-//         }
-//     }
-
-//     async login({email, password}) {
-//         try {
-//             return await this.account.createEmailSession(email, password);
-//         } catch (error) {
-//             throw error;
-//         }
-//     }
-
-    
-
-//     async getCurrentUser() {
-//         try {
-//             return await this.account.get();
-//         } catch (error) {
-//             console.log("Appwrite serive :: getCurrentUser :: error", error);
-//         }
-
-//         return null;
-//     }
-
-//     async logout() {
-
-//         try {
-//             await this.account.deleteSessions();
-//         } catch (error) {
-//             console.log("Appwrite serive :: logout :: error", error);
-//         }
-//     }
-// }
-
-// const authService = new AuthService();
-
-// export default authService
-
-
 import conf from '../conf/conf.js';
 import { Client, Account, ID } from "appwrite";
 import { Users, Client as node_client } from 'node-appwrite';
@@ -76,7 +12,6 @@ export class AuthService {
         this.client
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId)
-            // .setKey(conf.appwriteAPIKey); // Add the API key with `users.read` permission here
         this.account = new Account(this.client);
         this.node_client
         .setEndpoint(conf.appwriteUrl)
